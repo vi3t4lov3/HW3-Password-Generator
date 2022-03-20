7f156a08af7f79ac98930d443c1e7ee5b52cc7a9
Tu Nguyen <tuinfor@ymail.com>
2022-03-19 22:38:15 -0400

    declares the Characters

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//decares the variables of Characters
+var specialCharacters = '!%&,*+-./<>?~';
+var numbers ='0123456789';
+var lowerLetter = 'abcdefghijklmnopqrstuvwxyz';
+var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
